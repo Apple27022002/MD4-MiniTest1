1812eec19f3739aadd846239cd99d084a11e0852
Apple27022002 <100838894+Apple27022002@users.noreply.github.com>
2022-06-17 10:35:11 +0700

    done basic

---
+++ src/main/java/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post , Long> {
+
+    Iterable<Post> findAllByTitleContaining(String title);
+
+
+}
